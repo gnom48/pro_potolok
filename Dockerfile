685ee2349ebc0b493b4608a92a860e8705bfeb9c
Egor Ivanov <egorchima@gmail.com>
2024-10-19 18:22:45 +0300

    new docker config

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,32 @@
-FROM devries/bottle
+# FROM devries/bottle
 
-COPY requirements.txt requirements.txt
+# COPY requirements.txt requirements.txt
 
-EXPOSE 5555
+# EXPOSE 5555
 
-ADD . /app
+# ADD . /app
+# WORKDIR /app
+
+# CMD ["gunicorn","-b","0.0.0.0:5555","-w","3","--log-file","-","--log-level","debug","--access-logfile","-","app:app"]
+
+# Используем официальный образ Python
+# FROM python:3.9-slim
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.10
+
+# Устанавливаем рабочую директорию
 WORKDIR /app
 
-CMD ["gunicorn","-b","0.0.0.0:5555","-w","3","--log-file","-","--log-level","debug","--access-logfile","-","app:app"]
+# Копируем файл зависимостей
+COPY requirements.txt .
+
+# Устанавливаем зависимости
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Копируем весь код приложения в контейнер
+COPY . .
+
+# Открываем порт 5555
+EXPOSE 5555
+
+# Команда для запуска приложения
+CMD ["gunicorn", "-b", "0.0.0.0:5555", "-w", "3", "--log-file", "-", "--log-level", "debug", "--access-logfile", "-", "app:app"]
--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+# # @app.route('/static/<filepath:path>')
+# # def server_static(filepath):
+# #     return static_file(filepath, root=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static'))
+
+"""
+This script runs the application using a development server.
+"""
+
 import bottle
 import os
 import sys
 from bottle import static_file, request, response, run, Bottle
+from bottle import route, view, static_file
 from datetime import datetime
 import csv
 import smtplib
@@ -11,33 +20,47 @@ import schedule
 import time
 import threading
 import math
+import routes
 
-# Включение режима отладки, если задано
+# routes contains the HTTP handlers for our server and must be imported.
+import routes
+
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
+    # Debug mode will enable more verbose output in the console window.
+    # It must be set at the beginning of the script.
     bottle.debug(True)
 
-app = bottle.Bottle()
+# def wsgi_app():
+#     """Returns the application to make available through wfastcgi. This is used
+#     when the site is published to Microsoft Azure."""
+#     return bottle.default_app()
 
-# Путь к файлу для хранения данных о посетителях
+app = bottle.app()
+
+def run_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
 FILE_NAME = 'visitors.csv'
-
-# Инициализация файла для хранения данных о посетителях (если файл не существует)
 if not os.path.exists(FILE_NAME):
     with open(FILE_NAME, mode='w', newline='') as file:
         writer = csv.writer(file)
-        # Запись заголовков столбцов
         writer.writerow(["IP", "Начало сеанса", "Окончание сеанса", "Время на сайте (секунды)"])
     print(f"Создан файл {FILE_NAME}.")
 
-# Маршрут для главной страницы, где будет показана основная страница index.html
-@app.route('/')
-def index():
+scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+scheduler_thread.start()
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
     client_ip = request.remote_addr
     visit_time = datetime.now()
 
     print(f"Посетитель {client_ip} зашел на сайт в {visit_time}.")
 
-    # Запись визита в файл
     try:
         with open(FILE_NAME, mode='a', newline='') as file:
             writer = csv.writer(file)
@@ -46,13 +69,12 @@ def index():
     except Exception as e:
         print(f"Ошибка при записи в файл: {e}")
 
-    # Установка cookie для отслеживания времени на сайте
     response.set_cookie("session_start", visit_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
 
-    # Отображение основной страницы (index.html)
-    return static_file('index.html', root=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'views'))
+    return dict(
+        year=datetime.now().year
+    )
 
-# Маршрут для завершения сессии
 @app.route('/goodbye', method=['GET', 'POST'])
 def goodbye():
     session_start = request.get_cookie("session_start")
@@ -68,7 +90,6 @@ def goodbye():
         client_ip = request.remote_addr
         end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # Обновление последней записи для этого IP
         updated = False
         rows = []
         if os.path.exists(FILE_NAME):
@@ -87,14 +108,12 @@ def goodbye():
                 writer.writerows(rows)
             print(f"Обновлена запись для {client_ip} с временем выхода {end_time} и временем на сайте {time_spent_seconds} секунд.")
         else:
-            # Если запись не найдена, создаем новую
             with open(FILE_NAME, mode='a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow([client_ip, '', end_time, time_spent_seconds])
             print(f"Создана новая запись для {client_ip} с временем выхода {end_time} и временем на сайте {time_spent_seconds} секунд.")
     return ""
 
-# Функция для отправки отчета по почте каждые 24 часа на два email
 def send_email_report():
     if not os.path.exists(FILE_NAME):
         print("Файл с данными о посетителях не найден.")
@@ -102,46 +121,40 @@ def send_email_report():
 
     rows = []
     report = ""
-    total_visitors = 0  # Счетчик уникальных посетителей
-    unique_ips = set()  # Множество для хранения уникальных IP-адресов
+    total_visitors = 0  
+    unique_ips = set()
 
-    # Чтение данных о посетителях
     with open(FILE_NAME, mode='r', newline='') as file:
         reader = csv.reader(file)
-        # Пропускаем заголовок
         next(reader)
         for row in reader:
-            unique_ips.add(row[0])  # Добавляем IP-адрес в множество уникальных посетителей
+            unique_ips.add(row[0]) 
             
-            # Если сеанс не завершен (нет времени выхода), вычисляем текущее время на сайте
             if row[2] == '':
                 session_start = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S")
                 time_spent = datetime.now() - session_start
-                time_spent_seconds = math.ceil(time_spent.total_seconds())  # Округляем вверх
+                time_spent_seconds = math.ceil(time_spent.total_seconds())
                 row[3] = str(time_spent_seconds)
                 row[2] = "Сеанс не завершен"
             else:
-                # Преобразуем значение в целое число только если оно действительно является числом
                 try:
-                    time_spent_seconds = int(row[3])  # Получаем время, если сеанс завершен
+                    time_spent_seconds = int(row[3]) 
                 except ValueError:
                     print(f"Ошибка преобразования времени на сайте для IP {row[0]}: {row[3]}")
-                    time_spent_seconds = 0  # Или любое значение по умолчанию
+                    time_spent_seconds = 0 
 
             rows.append(row)
 
-            # Формируем отчет
             report += f"Посетитель {row[0]} провел на сайте {time_spent_seconds} секунд. Статус: {row[2]}.\n"
 
 
-    total_visitors = len(unique_ips)  # Общее количество уникальных посетителей
+    total_visitors = len(unique_ips) 
 
-    # Добавляем общее количество посетителей в начало отчета
     report = f"Общее количество посетителей Pro Потолок: {total_visitors}\n\n" + report
 
-    # Отправка email с отчетом
     sender_email = "dinardarsky@yandex.ru"
-    receiver_emails = ["dinardarskii@yandex.ru", "pro-potolok92@yandex.ru"]  # Список email получателей
+    # receiver_emails = ["egorchima@gmail.com"]
+    receiver_emails = ["dinardarskii@yandex.ru", "pro-potolok92@yandex.ru"]
     password = "zvcgbanemxayvdpn"
 
     for receiver_email in receiver_emails:
@@ -153,7 +166,6 @@ def send_email_report():
         message.attach(MIMEText(report, "plain"))
 
         try:
-            # Настройка SMTP-сервера
             server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
@@ -162,32 +174,16 @@ def send_email_report():
         except Exception as e:
             print(f"Ошибка при отправке на {receiver_email}: {e}")
 
-    # Удаление файла после успешной отправки отчета
     try:
         os.remove(FILE_NAME)
         print(f"Файл {FILE_NAME} успешно удален.")
     except Exception as e:
         print(f"Ошибка при удалении файла {FILE_NAME}: {e}")
 
-# Планирование отправки отчета в 12:01 каждый день
+
+# schedule.every().day.at("15:18").do(send_email_report)
 schedule.every().day.at("09:00").do(send_email_report)
 
-# Фоновая задача для выполнения планировщика
-def run_scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
-
-# Запуск планировщика в отдельном потоке
-scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
-scheduler_thread.start()
-
-# Запуск статических файлов (если необходимо)
-@app.route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static'))
-
-# Запуск приложения Bottle
 if __name__ == '__main__':
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
@@ -197,4 +193,5 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
 
-    run(app=app, host='localhost', port=8080, debug=True)
+    # Starts a local test server.
+    bottle.run(app=app, host='localhost', port='8080')
